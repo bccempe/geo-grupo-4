3a1ff4916dc2daca285a05244241366ade16fd6e
Nicolas Salinas <nicolas.salinas.r@usach.cl>
2026-04-10 13:55:57 -0400

    cargar datos desde drive

---
+++ backend/Cargar_datos/descargar_datos.py
@@ -0,0 +1,26 @@
+import subprocess
+from pathlib import Path
+
+FOLDER_ID = "1GDd0x2kXG-wzo3s48ar9c81za9obMleQ"
+
+
+def sincronizar_drive():
+    carpeta_destino = Path("/data")
+    carpeta_destino.mkdir(exist_ok=True)
+
+    comando = [
+        "rclone",
+        "copy",
+        "gdrive:",
+        str(carpeta_destino),
+        "--drive-root-folder-id",
+        FOLDER_ID,
+        "-P"
+    ]
+
+    subprocess.run(comando, check=True)
+    print("✅ Descarga completada")
+
+
+if __name__ == "__main__":
+    sincronizar_drive()
