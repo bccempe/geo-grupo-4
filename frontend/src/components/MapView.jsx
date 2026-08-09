f06bd5e26dae0c09cfd57438aeeee3876f3ecfed
Benjamín Canales Cempe <benjamin.canales.c@usach.cl>
2026-08-09 16:47:53 -0400

    fix proposed center marker icon url

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -21,7 +21,7 @@ const redOriginIcon = new L.Icon({
 });
 
 const goldProposedIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
